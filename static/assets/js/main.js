6f6d8b6c0337c7df6cb3735d16f1b056a65937e8
asadadsadfsa <abdielpala86@gmail.com>
2025-01-24 23:15:40 -0600

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -27,7 +27,7 @@ loadCSS('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icon
 });
 if(window.location.pathname === "/index.html" || window.location.pathname === "/") {
   console.log("[✔️] Index.html");
-  const options = ["Right-Click to access more features", "Set a custom background in settings.", "Tab Cloaking is highly recommended", "About:Blank Cloak is highly recommended", "This site was originally created as a joke", "Hosted on the best hardware!", "Do not share this link with anyone.", "Join our Discord for more cool things!", "Join our Discord for more cool things!", "© Copyright 2024 Doge Network. All Rights Reserved.", "Customize Doge by going to Settings > Themes", "Want more links? Our Discord provides tons of links!", "Want more links? Our Discord provides tons of links!", "Want more links? Our Discord provides tons of links!", "Hi, fowntain here!", ];
+  const options = ["Right-Click to access more features", "🔥", "Si quema cuh", "i love baby oil.", "ggezzz", "Abdiel the goat", "Lakeview mis hijos", "Set a custom background in settings.", "195.0.3.236", "Tab Cloaking is highly recommended", "About:Blank Cloak is highly recommended", "Hosted on the best hardware!", "Do not share this link with anyone.", "Join our Discord for more cool things!", "© Copyright 2024 Abdiel Network. All Rights Reserved.", "Customize Doge by going to Settings > Themes", "Want more links? Our Discord provides tons of links!", "Abdiel was here.", ];
 
   function getRandomOption() {
     const randomNumber = Math.floor(Math.random() * options.length);
